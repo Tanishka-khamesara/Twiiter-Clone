6975e5af5aee65f8d48d9fcf79bdd85dd4e1f28f
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-06-22 14:29:49 +0530

    trying

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
     queryKey: ["authUser"],
     queryFn: async () => {
       try {
-        const res = await fetch("http://localhost:8000/api/auth/me", {
+        const res = await fetch("https://twiiter-clone-1-ylzj.onrender.com/api/auth/me", {
           method: "GET",
           credentials: 'include',
         });
