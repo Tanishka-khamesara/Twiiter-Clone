1ab2ed5857bfef17fe8248861f47e49d6ee7cc73
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-06-24 14:48:20 +0530

    code

--- backend/server.js
+++ backend/server.js
@@ -20,17 +20,17 @@ cloudinary.config({
 });
 
 const app = express();
-const Port = process.env.PORT;
+const Port = process.env.PORT || 5000;
 
 const corsOptions = {
-    origin: 'https://twiiter-clone-hclr.vercel.app/login', // Base URL of your frontend application
+    origin: 'https://twiiter-clone-hclr.vercel.app', // Base URL of your frontend application
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
 
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
+app.use(cors(corsOptions)); // Enable CORS with specific options
+app.options('*', cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
