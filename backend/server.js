cb7c77e107508eb8996e033888a0ddbb9d7d6a4d
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-06-25 15:04:41 +0530

    authentication updated

--- backend/server.js
+++ backend/server.js
@@ -24,11 +24,11 @@ const Port = process.env.PORT || 5000;
 
 const corsOptions = {
     origin: ['http://localhost:3000',"https://twiiter-clone-hclr.vercel.app"] ,
-    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 };
 
 app.use(cors(corsOptions)); // Enable CORS with specific options
+app.options('*', cors(corsOptions));
 // app.options('*', cors(corsOptions)); // Handle preflight requests
 
 app.use(express.json());
