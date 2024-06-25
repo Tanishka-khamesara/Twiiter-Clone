7f0d7d620b2af66bea812598179b9e316909fcbf
Tanishka khamesara <tanishkajain24941@gmail.com>
2024-06-25 12:38:54 +0530

    code

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -109,7 +109,8 @@ export const login = async (req, res, next) => {
 export const logout = async (req, res) => {
     try {
         console.log("before",res.cookie.jwt)
-        res.cookie("jwt", "", { expiresIn: new Date(Date.now()) })
+        // res.cookie("jwt", "", { expiresIn: new Date(Date.now()) })
+        res.clearCookie("jwt");
         console.log("after",res.cookie.jwt)
         res.status(200).json({
             message:"Logged out Succesfully!"
